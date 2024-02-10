7f336625bffee272e93281b224f268d01aa7da51
Ramsai <ramsaicrony@gmail.com>
2024-02-10 11:31:18 +0530

    Removed border for HappyCustomers component

--- src/components/HappyCustomers.js
+++ src/components/HappyCustomers.js
@@ -2,15 +2,13 @@ import { HAPPY_CUSTOMERS_YOUTUBE_IDS, YOUTUBE_URL_PREFIX } from "../helper/const
 
 const HappyCustomers = () => {
     return (
-        <div className="w-full h-60 bg-gray-50 border border-black">
+        <div className="w-full h-72 bg-gray-50 p-2 m-2">
             <div className="items-center justify-center flex p-2 flex-col">
                 <span className="font-bold text-lg ">Happy Customers</span>
                 <span className="border border-gray-300 w-1/5 m-2"></span>
             </div>
             <div className="flex space-x-3 p-3 justify-between">
                 {HAPPY_CUSTOMERS_YOUTUBE_IDS.map((text, index) => <EmbeddSingleVideo key={index} text={text}/>)}
-                
-                
             </div>
         </div>
     )
